fe41586a23ab9dad89cd32d115bdcea6cb0f308e
Thomas Testa <ttesta99@yahoo.com>
2026-01-27 15:43:36 -0500

    implement fetch teachers and create classes

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import subjectsRouter from './routes/subjects';
 import securityMiddleware from './middleware/security';
 import { toNodeHandler } from "better-auth/node";
 import { auth } from './lib/auth';
+import usersRouter from './routes/users';
+import classesRouter from './routes/classes';
 
 const app = express();
 const PORT = 8000;
@@ -28,7 +30,9 @@ app.set("trust proxy", 1);
 app.use(express.json());
 app.use(securityMiddleware);
 
-app.use('/api/subjects', subjectsRouter)
+app.use('/api/subjects', subjectsRouter);
+app.use('/api/users', usersRouter);
+app.use("/api/classes", classesRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello, welcome to the Classroom API');
---
+++ src/routes/classes.ts
@@ -0,0 +1,29 @@
+import express from "express"
+import { db } from "../db";
+import { classes } from "../db/schema";
+
+const classesRouter = express.Router();
+
+classesRouter.post("/", async (req, res) => {
+    try {
+        const {name, teacherId, subjectId, capacity, description, status, bannerUrl, bannerCldPubId, } = req.body;
+
+        const [createdClass] = await db
+            .insert(classes)
+            .values({
+                ...req.body,
+                inviteCode: Math.random().toString(36).substring(2, 9),
+                schedules: []
+            })
+            .returning({id: classes.id});
+
+        if(!createdClass) throw Error;
+
+        res.status(201).json({data: createdClass});
+    } catch (error) {
+        console.error(`POST /classes error: ${error}`);
+        res.status(500).json({error: error});
+    }
+});
+
+export default classesRouter;
